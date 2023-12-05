e65232508587fced4249fe90a5254ce60f4f1389
Jeroen van Langen <jvanlangen@live.nl>
2023-12-05 09:09:28 +0100

    Fixed automatic reconnect

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,5 +1,6 @@
 export const connection = new signalR.HubConnectionBuilder()
     .withUrl('/colossushub')
+    .withAutomaticReconnect([0, 3000, 5000, 10000, 15000, 30000])
     .build()
 
 export function startSignalR() {
